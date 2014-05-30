1d02570630de6a9e274f1166fb7d97532dafb2a3
Anton Zherdev <anton.zherdev@gmail.com>
2014-05-30 13:34:56 +0400

    reorganize

---
+++ build.sh
@@ -0,0 +1,3 @@
+cabal install --prefix=$HOME --user
+cd ./ObjDLib/Sources/
+./dist/build/ObjD/ObjD --java ../Java/ObjDLib/gen/ --java-test ../Java/ObjDLib/gen-test/  --obj-c ../ObjC/ObjDLib/ObjDLib/Generated/
